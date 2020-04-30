c1906755f2dfdf0ef840808cd98d38870b1cfcb4
kennyhn <kennyhn@stud.ntnu.no>
2020-04-30 11:12:05 +0200

    code and figures prob6

--- code/problem6.m
+++ code/problem6.m
@@ -16,12 +16,12 @@ sigma_s_sq_hat  = sum(abs(sigma_s).^2)/K;
 lambda_prime    = (0:0.1:10000);
 
 mu_h0           = K*sigma_w_sq_hat;
-sigma_h0        = K*sigma_w_sq_hat^2;
+sigma_h0        = sqrt(K)*sigma_w_sq_hat;
 p_fa            = 1 - normcdf(lambda_prime,...
     mu_h0, sigma_h0);
 
 mu_h1           = K*(sigma_w_sq_hat+sigma_s_sq_hat);
-sigma_h1        = K*(sigma_w_sq_hat+sigma_s_sq_hat)^2;
+sigma_h1        = sqrt(K)*(sigma_w_sq_hat+sigma_s_sq_hat);
 p_d             = 1 - normcdf(lambda_prime,...
     mu_h1, sigma_h1);
 
@@ -44,3 +44,54 @@ xlabel('$\lambda''$',...
 ylabel('$p(\lambda'')$',...
     'Interpreter', 'latex', 'fontsize', 14);
 hold off
+
+%% Check out the gaussian distribution up against the gamma distribution
+x       = 700:0.1:1300;
+gaussh0 = normpdf(x, mu_h0, sigma_h0);
+gammah0 = gampdf(x, K, sigma_w_sq_hat);
+
+figure(2);
+title('Gauss vs Gamma distribution under $H_0$',...
+    'Interpreter', 'latex', 'fontsize', 22);
+hold on
+plot(x, gaussh0, 'Linewidth', 1);
+hold on
+plot(x, gammah0, '--', 'Linewidth', 1);
+hold on
+grid on;
+hold on
+legend('Gaussian','Gamma',...
+    'Interpreter', 'latex', 'fontsize', 18);
+hold on
+xlabel('$x$',...
+    'Interpreter', 'latex', 'fontsize', 14);
+ylabel('$p(x)$',...
+    'Interpreter', 'latex', 'fontsize', 14);
+hold off
+
+
+%% Under H1
+x         = 1500:0.1:2500;
+gaussh1   = normpdf(x, mu_h1, sigma_h1);
+gammah1   = gampdf(x, K, (sigma_w_sq_hat+sigma_s_sq_hat));
+
+figure(3);
+title('Gauss vs Gamma distribution under $H_1$',...
+    'Interpreter', 'latex', 'fontsize', 22);
+hold on
+plot(x, gaussh1, 'Linewidth', 1);
+hold on
+plot(x, gammah1, '--', 'Linewidth', 1);
+hold on
+grid on;
+hold on
+legend('Gaussian','Gamma',...
+    'Interpreter', 'latex', 'fontsize', 18);
+hold on
+xlabel('$x$',...
+    'Interpreter', 'latex', 'fontsize', 14);
+ylabel('$p(x)$',...
+    'Interpreter', 'latex', 'fontsize', 14);
+hold off
+
+
